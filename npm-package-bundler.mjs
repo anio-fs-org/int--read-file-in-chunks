9397523c9501a066f99c866c9249f00f90c38284
dasdeo <mark@anio.software>
2024-03-03 13:22:50 +0100

    Replace @anio-js-foundation/bundle-package with @anio-software/npm-package-bundler.

---
+++ npm-package-bundler.mjs
@@ -0,0 +1,18 @@
+export default {
+	preprocessing: [{
+		file: "./src/template.mjs",
+		items: {
+			"export default async function": "export default function",
+			"await fs_object.openFile(": "fs_object.openFile(",
+			"await file.read(": "file.read(",
+			"await file.close()": "file.close()",
+			"async readNextChunk() {": "readNextChunk() {",
+			"async close() {": "close() {"
+		},
+		output: "./src/auto/sync.mjs"
+	}, {
+		file: "./src/template.mjs",
+		items: {},
+		output: "./src/auto/async.mjs"
+	}]
+}
