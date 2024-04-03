6b6254c29ec94e98cd86078e6aed1f9e9997248c
eXory2024 <exory@anio.software>
2024-04-04 01:47:10 +0200

    Replace @anio-node-foundation/fs-api with @anio-fs/api.

--- src/index.mjs
+++ src/index.mjs
@@ -1,17 +1,14 @@
-import createFSObject from "@anio-node-foundation/fs-api"
+import fs from "@anio-fs/api"
 
 import sync_impl from "./auto/sync.mjs"
 import async_impl from "./auto/async.mjs"
 
-const async_fs = createFSObject({sync: false})
-const sync_fs = createFSObject({sync: true})
-
 function nodeFsReadFileInChunks(path, chunk_size) {
-	return async_impl(async_fs, path, chunk_size)
+	return async_impl(fs.async, path, chunk_size)
 }
 
 nodeFsReadFileInChunks.sync = function(path, chunk_size) {
-	return sync_impl(sync_fs, path, chunk_size)
+	return sync_impl(fs.sync, path, chunk_size)
 }
 
 export default nodeFsReadFileInChunks
